74b861b91538f8b6980318f7beb1e9a069ad7995
timehorizon <x@x.x>
2020-09-24 19:31:58 +0200

    add scss2json script, add package json, update and move gitignore

---
+++ scripts/scss2json.js
@@ -0,0 +1,24 @@
+// turn scss into json
+
+const fs = require("fs");
+
+const file = fs.readFileSync("./demo/scss/_variables.scss").toString();
+
+const out = {};
+
+file
+  .split("$beacons: (")[1]
+  .replace(",\n);", "")
+  .replace(/(\r\n|\n|\r)/gm, "") // rm line breaks
+  .replace(/\s+/g, "") // rm spaces
+  .split(",")
+  .forEach((beacon) => {
+    const split = beacon.split(":");
+    const key = split[0];
+    const val = split[1];
+    Object.assign(out, { [key]: val });
+  });
+
+fs.writeFile("./demo/beacons.json", JSON.stringify(out, null, 2), (err) => {
+  if (err) throw err;
+});
